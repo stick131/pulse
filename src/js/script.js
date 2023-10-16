2748ca5555bbd54360be6e452c6575ea0a0249dc
Ruabchickov Egor <egorr316@gmail.com>
2023-10-16 21:48:38 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,21 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 1200,
-      prevArrow: '<button type="button" class="slick-prev"><img src=images/left.svg></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src=images/right.svg></button>',
-      responsive:[
-          {
-            breakpoint: 768,
-            settings: {
-              arrows:false
-            }
-          },
-      ]
-    });
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    prevArrow: '<button type="button" class="slick-prev"><img src=images/left.svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src=images/right.svg></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
-  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab__active)', function () {
+    $(this)
+      .addClass('catalog__tab__active').siblings().removeClass('catalog__tab__active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content__active').eq($(this).index()).addClass('catalog__content__active');
+  });
+});
+
