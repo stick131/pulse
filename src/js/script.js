9d69a8b5018513d98e44162d591007f5be208b7b
Ruabchickov Egor <egorr316@gmail.com>
2023-10-20 21:25:02 +0300

    website is ready

--- src/js/script.js
+++ src/js/script.js
@@ -17,5 +17,84 @@ $(document).ready(function () {
       .addClass('catalog__tab__active').siblings().removeClass('catalog__tab__active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content__active').eq($(this).index()).addClass('catalog__content__active');
   });
+
+  // Modal
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+  $('.catalog-item__btn').on('click', function(){
+    $('.overlay, #order').fadeIn('slow');
+  });
+
+  // $('#consultation form').validate({
+  //   rules:{
+  //     name:{
+  //       required:true,
+  //       minlength: 3,
+  //     },
+  //     phone:"required",
+  //     email:{
+  //       required: true,
+  //       email: true,
+
+  //     }
+  //   },
+  //   messages:{
+  //     name:{
+  //       required:'Введите пожалуйста свое имя',
+  //       minlength: jQuery.validator.format("Введите {0} симовола!")
+  //     },
+  //     phone:'Введите пожалуйста свой номер телефона',
+  //     email:{
+  //       required:'Пожалуйста введите свою почту'        
+  //     }
+  //   }
+  // });
+ 
+
+  function validForms(form){
+    $(form).validate({
+      rules:{
+        name:{
+          required:true,
+          minlength: 3,
+        },
+        phone:"required",
+        email:{
+          required: true,
+          email: true,
+  
+        }
+      },
+      messages:{
+        name:{
+          required:'Введите пожалуйста свое имя',
+          minlength: jQuery.validator.format("Введите {0} симовола!")
+        },
+        phone:'Введите пожалуйста свой номер телефона',
+        email:{
+          required:'Пожалуйста введите свою почту'        
+        }
+      }
+    });
+  };
+  validForms('#consultation-form');
+  validForms('#consultation form');
+  validForms('#order form');
+
+  $('input[name=phone]').mask("+ 7 (999) 999-99-99");
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 1600){
+      $('.pageup').fadeIn();
+    }else{
+      $('.pageup').fadeOut();
+    }
+  });
+
+  new WOW().init();
 });
 
